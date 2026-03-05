84ec55b1a846bd88cde182f5b32a5311cf33f221
Manojkumar <manojkumardannana@gmail.com>
2026-03-05 22:06:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Portfolio Website Loaded");
+// particles background
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#3b82f6" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    move: { speed: 2 }
+  }
+});
+
+
+// scroll animation
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+sections.forEach(sec => {
+
+const top = sec.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+sec.classList.add("show");
+}
+
+});
+
+});
+
+
+// skill animation
+
+window.onload = () => {
+
+document.querySelectorAll(".fill").forEach(bar => {
+
+bar.style.width = bar.getAttribute("style").split(":")[1];
+
+});
+
+};
